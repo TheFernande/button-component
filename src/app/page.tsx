c15f1dd532f8e9162dd708b4c91710c41a158271
Jade Hernandez <jade.hernandez88@gmail.com>
2024-12-30 16:53:20 +0100

    Introduces isDisabled property to button variants and updates button component to handle disabled state. Removes iconColor property and adjusts styles accordingly. Adds responsive styles for mobile screens.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,91 +8,99 @@ import {
   buttonVariantsF
 } from "./buttons.data";
 
+import "../styles/button.css";
+
 export default function Home() {
   return (
     // `<main>` -> this is the "Page body" element in the Figma design
     <main className='gfe-main'>
-      <div className='flex flex-wrap items-center justify-start gap-5 py-12'>
+      {/* Variant A */}
+      <div className='mobileGfe:max-w-[323px] flex max-w-full flex-wrap items-center justify-start gap-5 py-6 md:py-12'>
         {buttonVariantsA.map(button => (
           <Button
             key={button.key}
             label={button.label}
             classNames={button.classNames}
+            isDisabled={button.isDisabled}
             withText={button.withText}
             withIcon={button.withIcon}
             iconPosition={button.iconPosition}
             iconSize={button.iconSize}
-            iconColor={button.iconColor}
           />
         ))}
       </div>
-      <div className='flex flex-wrap items-center justify-start gap-5 py-12'>
+      {/* Variant B */}
+      <div className='mobileGfe:max-w-[323px] flex max-w-full flex-wrap items-center justify-start gap-5 py-6 md:py-12'>
         {buttonVariantsB.map(button => (
           <Button
             key={button.key}
             label={button.label}
             classNames={button.classNames}
+            isDisabled={button.isDisabled}
             withText={button.withText}
             withIcon={button.withIcon}
             iconPosition={button.iconPosition}
             iconSize={button.iconSize}
-            iconColor={button.iconColor}
           />
         ))}
       </div>
-      <div className='flex flex-wrap items-center justify-start gap-5 py-12'>
+      {/* Variant C */}
+      <div className='mobileGfe:max-w-[323px] flex max-w-full flex-wrap items-center justify-start gap-5 py-6 md:py-12'>
         {buttonVariantsC.map(button => (
           <Button
             key={button.key}
             label={button.label}
             classNames={button.classNames}
+            isDisabled={button.isDisabled}
             withText={button.withText}
             withIcon={button.withIcon}
             iconPosition={button.iconPosition}
             iconSize={button.iconSize}
-            iconColor={button.iconColor}
           />
         ))}
       </div>
-      <div className='flex flex-wrap items-center justify-start gap-5 py-12'>
+      {/* Variant D */}
+      <div className='mobileGfe:max-w-[323px] flex max-w-full flex-wrap items-center justify-start gap-5 py-6 md:py-12'>
         {buttonVariantsD.map(button => (
           <Button
             key={button.key}
             label={button.label}
             classNames={button.classNames}
+            isDisabled={button.isDisabled}
             withText={button.withText}
             withIcon={button.withIcon}
             iconPosition={button.iconPosition}
             iconSize={button.iconSize}
-            iconColor={button.iconColor}
           />
         ))}
       </div>
-      <div className='flex flex-wrap items-center justify-start gap-5 py-12'>
+      {/* Variant E */}
+      <div className='mobileGfe:max-w-[323px] flex max-w-full flex-wrap items-center justify-start gap-5 py-6 md:py-12'>
         {buttonVariantsE.map(button => (
           <Button
             key={button.key}
             label={button.label}
             classNames={button.classNames}
+            isDisabled={button.isDisabled}
             withText={button.withText}
             withIcon={button.withIcon}
             iconPosition={button.iconPosition}
             iconSize={button.iconSize}
-            iconColor={button.iconColor}
           />
         ))}
       </div>
-      <div className='flex flex-wrap items-center justify-start gap-5 py-12'>
+      {/* Variant F */}
+      <div className='mobileGfe:max-w-[323px] flex max-w-full flex-wrap items-center justify-start gap-5 py-6 md:py-12'>
         {buttonVariantsF.map(button => (
           <Button
             key={button.key}
             label={button.label}
             classNames={button.classNames}
+            isDisabled={button.isDisabled}
             withText={button.withText}
             withIcon={button.withIcon}
             iconPosition={button.iconPosition}
             iconSize={button.iconSize}
-            iconColor={button.iconColor}
           />
         ))}
       </div>
--- src/components/button-component.tsx
+++ src/components/button-component.tsx
@@ -4,28 +4,31 @@ export interface IButtonProps {
   key: string;
   label: string;
   classNames: string;
+  isDisabled?: boolean;
   withText?: boolean;
   withIcon?: boolean;
   iconPosition?: "left" | "right";
   iconSize?: 20 | 24;
-  iconColor?: "#FFF" | "#525252" | "#000" | "#4338CA";
 }
 
 export default function Button({
   label,
   classNames,
+  isDisabled = false,
   withText = true,
   withIcon = false,
   iconPosition = undefined,
-  iconSize = 20,
-  iconColor = "#4338CA"
+  iconSize = 20
 }: Readonly<IButtonProps>) {
   return (
-    <div className={"flex items-center font-medium" + " " + classNames}>
+    <button
+      className={"flex items-center font-medium" + " " + classNames}
+      disabled={isDisabled}
+    >
       {withIcon && iconPosition === "left" ? (
         <IconStar
           size={iconSize}
-          color={iconColor}
+          isDisabled={isDisabled}
         />
       ) : null}
 
@@ -34,16 +37,16 @@ export default function Button({
       {withIcon && iconPosition === "right" ? (
         <IconStar
           size={iconSize}
-          color={iconColor}
+          isDisabled={isDisabled}
         />
       ) : null}
 
       {!withText && withIcon && iconPosition === undefined ? (
         <IconStar
           size={iconSize}
-          color={iconColor}
+          isDisabled={isDisabled}
         />
       ) : null}
-    </div>
+    </button>
   );
 }
